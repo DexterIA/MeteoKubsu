4216a0e4184ea10f30d9ad233c12e8c6dc09c444
Alexey.Bushkov <Alexey.Bushkov@billing.ru>
2015-08-11 17:40:01 +0300

    change 5 ngRoute

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'main.ctrl', 'year.ctrl', 'day.ctrl', 'month.ctrl']);
+var app = angular.module('starter', ['ionic', 'main.ctrl', 'year.ctrl', 'day.ctrl', 'month.ctrl', 'ngRoute']);
 
     app.run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
@@ -17,44 +17,23 @@ var app = angular.module('starter', ['ionic', 'main.ctrl', 'year.ctrl', 'day.ctr
             }
         });
     });
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
-        $stateProvider
-            .state('main', {
-                url: '/',
-                views: {
-                    'main': {
-                        templateUrl: 'templates/main_page.html',
-                        controller: 'main_ctrl'
-                    }
-                }
-            })
-            .state('day', {
-                url: '/day',
-                views: {
-                    'main-day': {
-                        templateUrl: 'templates/day_temp.html',
-                        controller: 'day_ctrl'
-                    }
-                }
-            })
-            .state('month', {
-                url: '/month',
-                views: {
-                    'main-month': {
-                        templateUrl: 'templates/month_temp.html',
-                        controller: 'month_ctrl'
-                    }
-                }
-            })
-            .state('year', {
-                url: '/year',
-                views: {
-                    'main-year': {
-                        templateUrl: 'templates/year_temp.html',
-                        controller: 'year_ctrl'
-                    }
-                }
-            });
-    });
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider
+                .when('/day', {
+                    templateUrl: 'templates/day_temp.html',
+                    controller: 'day_ctrl'
+                })
+                .when('/month',{
+                    templateUrl: 'templates/month_temp.html',
+                    controller: 'month_ctrl'
+                })
+                .when('/year',{
+                    templateUrl: 'templates/year_temp.html',
+                    controller: 'year_ctrl'
+                })
+                .otherwise({
+                    redirectTo: '/day'
+                });
+        }]);
 
