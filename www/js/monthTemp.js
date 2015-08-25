e0800a8a3af422a3fa57e2c0fd7ace58ed1288d7
DexterIA <dexterrrrr@list.ru>
2015-08-25 07:28:11 +0300

    v2.02 fix label

--- www/js/monthTemp.js
+++ www/js/monthTemp.js
@@ -1,6 +1,9 @@
 angular.module('month.ctrl',['ionic'])
     .controller("month_ctrl", function ($scope, $http, $ionicPopup, getMonthData, $routeParams) {
         $scope.refreshMonth = function () {
+            var dd = new Date(parseInt($routeParams.sd));
+            $scope.monthName = dd.getMonthName();
+            $scope.year = dd.getFullYear();
             var monthData = getMonthData.all($routeParams.sd, $routeParams.ed);
             $(function () {
                 $('#container').highcharts({
@@ -39,3 +42,20 @@ angular.module('month.ctrl',['ionic'])
         };
         $scope.refreshMonth();
         });
+
+Date.prototype.getMonthName = function(){
+    switch (this.getMonth()) {
+        case 0 :{return "Январь"}
+        case 1 :{return "Февраль"}
+        case 2 :{return "Март"}
+        case 3 :{return "Апрель"}
+        case 4 :{return "Май"}
+        case 5 :{return "Июнь"}
+        case 6 :{return "Июль"}
+        case 7 :{return "Август"}
+        case 8 :{return "Сентябрь"}
+        case 9 :{return "Октябрь"}
+        case 10 :{return "Ноябрь"}
+        case 11 :{return "Декабрь"}
+    }
+};
